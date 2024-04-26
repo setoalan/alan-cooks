b14995214ef3ff8724dd2170b697f07cb062cef8
Alan Seto <me@alanseto.com>
2024-04-26 08:04:17 -0500

    Install sanity-plugin-vercel-deploy

--- platform/sanity.config.js
+++ platform/sanity.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy';
 import schemaTypes from './schemas';
 
 export default defineConfig({
   title: 'alan-cooks',
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
-  plugins: [deskTool()],
+  plugins: [deskTool(), vercelDeployTool()],
   schema: {
     types: schemaTypes,
   },
--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <ThemeProvider theme={theme}>
       <Nav />
-      <Container maxWidth="lg" sx={{ px: { xs: 2 } }}>
+      <Container maxWidth="lg" sx={{ px: { xs: 2 }, pb: { xs: 2, sm: 4 } }}>
         <main>{children}</main>
       </Container>
     </ThemeProvider>
