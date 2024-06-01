37e6a7ba8be00d701a1aaf7370f30b4b68d12e73
Alan Seto <me@alanseto.com>
2024-06-01 14:39:49 -0500

    Fix production minified React errors

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -1,6 +1,14 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import Layout from './src/components/Layout';
+
+export function replaceHydrateFunction() {
+  return (element, container) => {
+    const root = ReactDOM.createRoot(container);
+    root.render(element);
+  };
+}
 
 export function wrapPageElement({ element, props }) {
   return (
