a3a45975711b84596b942dc57dbd1ef16f857590
Alan Seto <me@alanseto.com>
2025-08-08 09:14:23 -0500

    Upgrade sanity & styled-components and replace deploy widget

--- platform/sanity.config.js
+++ platform/sanity.config.js
@@ -1,13 +1,19 @@
+import { dashboardTool } from '@sanity/dashboard';
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import { vercelDeployTool } from 'sanity-plugin-vercel-deploy';
+import { vercelWidget } from 'sanity-plugin-dashboard-widget-vercel';
 import schemaTypes from './schemas';
 
 export default defineConfig({
   title: 'alan-cooks',
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
-  plugins: [deskTool(), vercelDeployTool()],
+  plugins: [
+    deskTool(),
+    dashboardTool({
+      widgets: [vercelWidget()],
+    }),
+  ],
   schema: {
     types: schemaTypes,
   },
